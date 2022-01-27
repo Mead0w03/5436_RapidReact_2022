d0a1203638ae07e09ee5691690d0510efdf96c75
cybercatsgithub5436@gmail.com <schsrobotics6543>
2022-01-26 19:07:48 -0500

    add library

---
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -0,0 +1,73 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveBase;
+
+public class DriveCommands extends CommandBase{
+    private DriveBase driveBase;
+    private Joystick stick;
+
+
+    public DriveCommands() {
+
+    }
+
+    public DriveCommands(DriveBase driveBase, Joystick joy) {
+
+        this.addRequirements(driveBase);
+        this.driveBase = driveBase;
+        this.stick = joy;
+    }
+
+    @Override
+    public void initialize() {
+        
+    }
+
+    @Override
+    public void execute() {
+
+        double xOutput = stick.getRawAxis(0);
+        double yOutput = stick.getRawAxis(1);
+
+        if(xOutput > .3) {
+            xOutput *= .5;
+         
+         }
+         
+         if(yOutput > .3) {
+             yOutput *= .5;
+         }
+
+        double L = driveBase.driveProcessing(driveBase.deaden(xOutput, .1), driveBase.deaden(yOutput, .1), "Left")[0];
+        double R = driveBase.driveProcessing(driveBase.deaden(xOutput, .1), driveBase.deaden(yOutput, .1), "Right")[1];
+
+       
+
+
+        SmartDashboard.putNumber("Left: ", L);
+        SmartDashboard.putNumber("Right: ", R);
+        SmartDashboard.putNumber("Y axis: ", stick.getRawAxis(1));
+        SmartDashboard.putNumber("X axis: ", stick.getRawAxis(0));
+
+        //double R = -.1;
+        //double L = .1;
+        driveBase.drive(L, R);
+
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        //driveBase.stopAllDrivetrainMotors();
+    }
+  
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
+  
+}
---
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,92 @@
+package frc.robot.subsystems;
+
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveBase extends SubsystemBase {
+    private CANSparkMax motorFL;
+    private CANSparkMax motorFR;
+    private CANSparkMax motorBL;
+    private CANSparkMax motorBR;
+
+    private double xAxisDeadband;
+    private double yAxisDeadband;
+
+    public DriveBase() {
+      motorFR = new CANSparkMax(1, MotorType.kBrushless);
+      motorFL = new CANSparkMax(3, MotorType.kBrushless);
+      motorBR = new CANSparkMax(2, MotorType.kBrushless);
+      motorBL = new CANSparkMax(4, MotorType.kBrushless);
+    }
+
+    public double deaden(double input, double deadband) {
+        deadband = Math.min(1, deadband);
+        deadband = Math.max(0, deadband);
+       
+        if (Math.abs(input) - deadband < 0) {
+          return 0;
+        }
+        else {
+          return Math.signum(input) * ((Math.abs(input) - deadband) / (1 - deadband));
+        }
+    }
+      
+
+    public void init() {
+
+        motorFL.clearFaults();
+        motorBL.clearFaults();
+        motorFR.clearFaults();
+        motorBR.clearFaults();
+    }  //End of init method
+  
+  
+  
+  
+  
+    //Drive Command to send values to DriveBase Motors
+    public void drive(double l, double r){
+      motorFL.set(l);
+      motorBL.set(l);
+      motorFR.set(r);
+      motorBR.set(r);
+    }  //End of driveRobot
+  
+    public double[] driveProcessing(double xAxis, double yAxis, String direction){
+        double returnValue[] = {0,0};
+        double strafe = xAxis;
+        double fwd = -yAxis;  //Invert Flightstick values as forward is negative
+    
+        //First check to see if input is within deadzone
+        strafe = deaden(strafe, xAxisDeadband);
+        fwd = deaden(fwd, yAxisDeadband);
+        
+        //Bind the output to insure it is not above 100%
+        double L = boundSpeed(fwd + strafe);
+        double R = boundSpeed(fwd - strafe);
+    
+        // R = Robot.rightSideRequiresInversion? -R: R;  
+        // I inverted motor controller in constructor for right side so no need to invert right side
+        
+        returnValue[0] = L;
+        returnValue[1] = R;
+        return returnValue;
+    } // End of driveProcessing
+
+    //Quick Binding of speed to not go above 100%
+  public double boundSpeed(double inputSpeed){
+    return (Math.abs(inputSpeed) >= 1? Math.signum(inputSpeed)*1:inputSpeed);
+  }
+  
+    public void stopAllDrivetrainMotors(){
+      motorFL.set(0);
+      motorFR.set(0);
+      motorBL.set(0);
+      motorBR.set(0);
+    }//End of stopAllDrivetrainMotors()
+    
+}
+
