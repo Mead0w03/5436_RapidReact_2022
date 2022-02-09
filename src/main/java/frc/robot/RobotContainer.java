946a67004802fa6d702cfabe596e2cf6fefbb8c7
Op41Master <mgemel5576@rcs-k12.us>
2022-02-08 19:37:56 -0500

    I put all of the working shooter commands with velocity inot the main class.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,17 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+// imports for shooter commands
+import frc.robot.commands.ShooterCommands.CommandActivateShooter;
+import frc.robot.commands.ShooterCommands.CommandReverseShooter;
+import frc.robot.commands.ShooterCommands.CommandStopShooter;
+import frc.robot.commands.ShooterCommands.CommandFarHigh;
+import frc.robot.commands.ShooterCommands.CommandCloseLow;
+//close high has not been created but drivers say it is a position
+//import frc.robot.commands.ShooterCommands.CommandCloseHigh;
+import frc.robot.subsystems.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,13 +30,43 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  // init buttons
+  private final XboxController xboxController = new XboxController(0);
+  private final JoystickButton rightBumper = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
+  private final JoystickButton leftBumper = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
+  private final JoystickButton xButton = new JoystickButton(xboxController, XboxController.Button.kX.value);
+  private final JoystickButton bButton = new JoystickButton(xboxController, XboxController.Button.kB.value);
+  private Shooter shooter = new Shooter();
+
+  // shooter commands
+  private final CommandActivateShooter commandActivateShooter = new CommandActivateShooter(shooter);
+  private final CommandStopShooter commandStopShooter = new CommandStopShooter(shooter);
+  private final CommandReverseShooter commandReverseShooter = new CommandReverseShooter(shooter);
+  private final CommandFarHigh commandFarHigh = new CommandFarHigh(shooter);
+  private final CommandCloseLow commandCloseLow = new CommandCloseLow(shooter);
+  //close high has not been created but drivers say it is a position
+  //private final CommandCloseLow commandCloseHigh = new CommandCloseLow(shooter);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {
+    
+    // Shooter:
+      rightBumper.whileHeld(commandReverseShooter)
+                 .whenReleased(commandStopShooter);
+      leftBumper.whileHeld(commandActivateShooter)
+                  .whenReleased(commandStopShooter);
+    // Speed for shooter
+      xButton.whenPressed(commandFarHigh);
+      bButton.whenPressed(commandCloseLow);  
+      //close high has not been created but drivers say it is a position
+      //aButton.whenPressed(commandCloseHigh);  
   }
 
   /**
@@ -34,7 +75,6 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
